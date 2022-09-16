5fb11fc81c396e43bf1f7a9280cb0fa005fcea3c
ddld93 <umar.jere@gmail.com>
2022-09-16 02:01:16 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	controller "github.com/ddld93/abedmis/src/controllers"
+	"github.com/ddld93/abedmis/src/middleware"
+	"github.com/ddld93/abedmis/src/routes"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+
+	_ "github.com/ddld93/abedmis/docs"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file >>> ", err)
+	}
+
+}
+
+// @title           Swagger Example API
+
+func main() {
+	port := os.Getenv("PORT")
+	host := os.Getenv("DATABASE_HOST")
+	if host == "" {
+		host = "database"
+	}
+	if port == "" {
+		port = "3000"
+	}
+	BenenCtrl := controller.ConnectDB(host, 27017)
+	route := routes.BeneRoute{BenenCtrl: BenenCtrl}
+	router := gin.New()
+	//router.Use(middleware.Logger())
+	v1 := router.Group("/api/v1")
+	v1.Use(middleware.Auth())
+		{
+			v1.POST("/", route.AddOne())
+			v1.POST("/batch", route.AddMany())
+			v1.GET("/", route.GetAll())
+			v1.GET("/:id", route.GetOne())
+			// v1.POST("/post", route.FormTest())
+		}
+
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	err := router.Run(fmt.Sprintf("%s:%v", "0.0.0.0", port))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Server running on port %s", port)
+}
+//...
---
+++ src/middleware/Auth.go
@@ -0,0 +1,72 @@
+package middleware
+
+import (
+	"errors"
+	"net/http"
+	"time"
+	"github.com/gin-gonic/gin"
+)
+
+type Header struct {
+	API_KEY string `header:"api-key"`
+}
+type Keys struct {
+	PSP       string
+	Test_Keys string
+	Live_Keys string
+}
+
+var PSP = map[string]Keys{
+	"upperLink": {
+		PSP:       "upperLink",
+		Test_Keys: "testingupperLink",
+		Live_Keys: "livingupperLink",
+	},
+	"visualICT": {
+		PSP:       "visualICT",
+		Test_Keys: "testingvisualICT",
+		Live_Keys: "livingvisualICT",
+	},
+}
+
+func Auth() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		t := time.Now()
+		if len(c.GetHeader("api-key")) == 0 {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"Status":  http.StatusBadRequest,
+				"Message": "Require auth key",
+				"Payload": nil,
+				"Error":   errors.New("auth key not found"),
+			})
+			return
+		}
+		for _, v := range PSP {
+			if c.GetHeader("api-key") == v.Live_Keys {
+				c.Request.Header.Add("mode", "live")
+				c.Request.Header.Add("PSP", v.PSP)
+				c.Next()
+				latency := time.Since(t)
+				c.Writer.WriteString(latency.String())
+				return
+
+			}
+			if c.GetHeader("api-key") == v.Test_Keys {
+				c.Request.Header.Add("mode", "test")
+				c.Request.Header.Add("PSP", v.PSP)
+				c.Next()
+				latency := time.Since(t)
+				c.Writer.WriteString(latency.String())				
+				return
+			}
+
+		}
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"Status":  http.StatusBadRequest,
+			"Message": "Invalid api key",
+			"Payload": nil,
+			"Error":   errors.New("invalid api key"),
+		})
+	}
+}
